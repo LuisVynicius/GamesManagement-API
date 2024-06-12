061a0d8982afb5058a20078a4e37acfbebf189ce
LuisVynicius <luismevy@gmail.com>
2024-06-12 17:54:51 -0300

    Created Entities: Game and User

---
+++ gamesapi/src/main/java/com/mevy/gamesapi/entities/Game.java
@@ -0,0 +1,38 @@
+package com.mevy.gamesapi.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_game")
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Game implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true, updatable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 75)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    private Instant date;
+
+}
---
+++ gamesapi/src/main/java/com/mevy/gamesapi/entities/User.java
@@ -0,0 +1,47 @@
+package com.mevy.gamesapi.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_user", indexes = {
+    @Index(name = "IDX_Email", columnList = "email")
+})
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class User implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true, updatable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+}
