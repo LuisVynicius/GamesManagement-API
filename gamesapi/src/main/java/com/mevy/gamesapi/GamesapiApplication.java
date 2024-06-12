456593f2c41426cd5abe3f7d3b231265e7bda114
LuisVynicius <luismevy@gmail.com>
2024-06-12 17:15:21 -0300

    Created Project

---
+++ gamesapi/src/main/java/com/mevy/gamesapi/GamesapiApplication.java
@@ -0,0 +1,13 @@
+package com.mevy.gamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesapiApplication.class, args);
+	}
+
+}
