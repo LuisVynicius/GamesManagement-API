f1f0635264494290d7eb6e1991a6eadc74ee48b2
LuisVynicius <luismevy@gmail.com>
2024-06-12 18:10:53 -0300

    Updated Entity: Add Indexer to Game

--- gamesapi/src/main/java/com/mevy/gamesapi/entities/Game.java
+++ gamesapi/src/main/java/com/mevy/gamesapi/entities/Game.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -15,7 +16,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "tb_game")
+@Table(name = "tb_game", indexes = {
+    @Index(name = "IDX_Name", columnList = "name")
+})
 @NoArgsConstructor
 @Getter
 @Setter
