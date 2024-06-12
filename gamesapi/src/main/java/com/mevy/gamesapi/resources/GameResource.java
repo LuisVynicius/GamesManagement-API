16b0a139153d5a2a01cc0c867eb6e8e3e89dfe9d
LuisVynicius <luismevy@gmail.com>
2024-06-12 19:19:56 -0300

    Some Fixes

--- gamesapi/src/main/java/com/mevy/gamesapi/resources/GameResource.java
+++ gamesapi/src/main/java/com/mevy/gamesapi/resources/GameResource.java
@@ -1,5 +1,16 @@
 package com.mevy.gamesapi.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mevy.gamesapi.services.GameService;
+
+@RestController
+@RequestMapping("/game")
 public class GameResource {
     
+    @Autowired
+    private GameService gameService;
+
 }
--- gamesapi/src/main/java/com/mevy/gamesapi/resources/UserResource.java
+++ gamesapi/src/main/java/com/mevy/gamesapi/resources/UserResource.java
@@ -1,5 +1,16 @@
 package com.mevy.gamesapi.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mevy.gamesapi.services.UserService;
+
+@RestController
+@RequestMapping("/game")
 public class UserResource {
     
+    @Autowired
+    private UserService userService;
+
 }
